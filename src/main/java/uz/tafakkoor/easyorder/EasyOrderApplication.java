2f6a77b333234b4f16e8d731fc5a6e1a98e1ad94
khayr <abdu4ik5101@mail.com>
2023-04-10 11:25:48 +0500

    test

--- src/main/java/uz/tafakkoor/easyorder/EasyOrderApplication.java
+++ src/main/java/uz/tafakkoor/easyorder/EasyOrderApplication.java
@@ -10,6 +10,7 @@ public class EasyOrderApplication {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("Merge Hell safe branch hopefully");
         SpringApplication.run(EasyOrderApplication.class, args);
     }
 
