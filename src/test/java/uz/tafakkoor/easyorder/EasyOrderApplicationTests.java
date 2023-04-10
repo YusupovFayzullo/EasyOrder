2ceed04a1317e2ba84f2f1ba13ac485e18f84b6c
khayr <abdu4ik5101@mail.com>
2023-04-10 09:59:05 +0500

    Initial Commit

---
+++ src/test/java/uz/tafakkoor/easyorder/EasyOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.tafakkoor.easyorder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyOrderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
