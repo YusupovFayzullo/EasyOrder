a2d22f1b1392aa2ad7e04deb73e80ed5a4dad6c6
khayr <abdu4ik5101@mail.com>
2023-04-10 10:58:26 +0500

    test

--- src/main/java/uz/tafakkoor/easyorder/EasyOrderApplication.java
+++ src/main/java/uz/tafakkoor/easyorder/EasyOrderApplication.java
@@ -1,13 +1,22 @@
 package uz.tafakkoor.easyorder;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class EasyOrderApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(EasyOrderApplication.class, args);
+    }
+
+    @Bean
+    CommandLineRunner runner() {
+        return args -> {
+
+        };
     }
 
 }
