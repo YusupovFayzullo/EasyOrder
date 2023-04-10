70f57e88fb3f4b5b51a2ed0481572e54fe52e801
khayr <abdu4ik5101@mail.com>
2023-04-10 11:11:27 +0500

    test

--- src/main/java/uz/tafakkoor/easyorder/EasyOrderApplication.java
+++ src/main/java/uz/tafakkoor/easyorder/EasyOrderApplication.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.Bean;
 public class EasyOrderApplication {
 
     public static void main(String[] args) {
+        System.out.println("Hello World!");
         SpringApplication.run(EasyOrderApplication.class, args);
     }
 
